0a199ec873340a842d056b55cc61cd132ef3c858
abutoku <ystkz2021@gmail.com>
2022-01-07 14:37:17 +0900

    update

--- main.py
+++ main.py
@@ -175,7 +175,44 @@
 
 # <4 > リスト内包表記
 
-price = [str(i) + '円' for i in range(0,101)]
-print(price)
+# price = [str(i) + '円' for i in range(0,101)]
+# print(price)
 
+# <1 > 写経してみよう
+
+# ----function 関数--------------------------
+
+###Basic
+
+# def add(a,b):
+#   return a + b
+
+# result = add(10,20)
+# print(result)
+
+### Argument default value
+
+# def pow(a,b=2):
+#   return a ** b
+
+# result = pow(10)
+# print(result)
+
+###keyword arguments
+
+# def create_date(
+#   year=0,month=0,date=0):
+#   return str(year) + "/" + str(month) + "/" + str(date)
+
+# print(create_date(year=2017,date=10))
+
+###Multiple return 
+
+# def get_user():
+#   return 'Yohei',30
+
+# name,age = get_user()
+
+# print(name)
+# print(age)
 
